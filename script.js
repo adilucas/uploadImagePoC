ad32c0882c22834787600016be1c6695d9bbd1ef
alukasik <adrian.lukasik@dreamlab.pl>
2017-11-27 11:12:35 +0100

    Initial commit - fully presentative PoC

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var app = angular.module("billplease", ["naif.base64"]);
+app.controller("mainCtrl", function ($scope) {
+    initReceipt();
+
+    $scope.receipts = [];
+    $scope.modalShow = false;
+    $scope.invalidAlert = false;
+
+    $scope.addReceipt = function () {
+        $scope.invalidAlert = false;
+        if (!$scope.receiptFile.base64 || !$scope.receiptName || !$scope.shopName || !$scope.purchaseTime || !$scope.warrantyRange || !$scope.warrantyType) {
+            document.getElementById('modal-main').scrollTop = 0;
+            $scope.invalidAlert = true;
+        } else {
+            var newRec = {
+                image: $scope.receiptFile.base64,
+                name: $scope.receiptName,
+                shop: $scope.shopName,
+                purchaseTime: $scope.purchaseTime,
+                warrantyRange: $scope.warrantyRange,
+                warrantyType: $scope.warrantyType
+            };
+
+            if ($scope.receiptDesc) newRec.description = $scope.receiptDesc;
+
+            $scope.receipts.push(newRec);
+            $scope.modalShow = false;
+            initReceipt();
+            /** TODO: save to db */
+        }
+    };
+
+    function initReceipt() {
+        $scope.receiptFile = null;
+        $scope.receiptName = '';
+        $scope.shopName = '';
+        $scope.purchaseTime = null;
+        $scope.warrantyRange = '';
+        $scope.warrantyType = '';
+        $scope.receiptDesc = '';
+    }
+});
